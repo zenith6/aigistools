ae7dcd30c20f50669eb1a570f3dcee03f119ba1d
Seiji Nitta <zenith6@gmail.com>
2015-02-08 12:22:52 +0900

    漆黒の射手リタさんがチラチラ見てくるを追加

--- app/tools.js
+++ app/tools.js
@@ -16,4 +16,9 @@ module.exports = [
     id: 'treasure-fragment-timer',
     path: 'treasure-fragment-timer',
   },
+  {
+    name: '漆黒の射手リタさんがチラチラ見てくる',
+    id: 'aigisrita',
+    path: 'https://chrome.google.com/webstore/detail/ememknmjckfbganihedbodegbdddlhhl',
+  },
 ];
